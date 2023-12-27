83a00345048880a2a5704c20ffbdc57dbe7b95df
DivadanAr <divadan.arya12@gmail.com>
2023-12-27 21:16:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+data = [{
+    'img1': 'assets/Landingpage.png',
+    'img2': 'assets/Landingpage.png',
+    'title': 'Web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate repellendus commodi nam adipisci magni, optio eius possimus repudiandae facilis odit a inventore perspiciatis ex rem ab assumenda vel quae."
+}, {
+    'img1': 'assets/Landingpage.png',
+    'img2': 'assets/Landingpage.png',
+    'title': 'Web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate repellendus commodi nam adipisci magni, optio eius possimus repudiandae facilis odit a inventore perspiciatis ex rem ab assumenda vel quae."
+}, {
+    'img1': 'assets/Landingpage.png',
+    'img2': 'assets/Landingpage.png',
+    'title': 'Web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate repellendus commodi nam adipisci magni, optio eius possimus repudiandae facilis odit a inventore perspiciatis ex rem ab assumenda vel quae."
+}, {
+    'img1': 'assets/Landingpage.png',
+    'img2': 'assets/Landingpage.png',
+    'title': 'Web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate repellendus commodi nam adipisci magni, optio eius possimus repudiandae facilis odit a inventore perspiciatis ex rem ab assumenda vel quae."
+}, {
+    'img1': 'assets/Landingpage.png',
+    'img2': 'assets/Landingpage.png',
+    'title': 'Web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate repellendus commodi nam adipisci magni, optio eius possimus repudiandae facilis odit a inventore perspiciatis ex rem ab assumenda vel quae."
+}]
+
